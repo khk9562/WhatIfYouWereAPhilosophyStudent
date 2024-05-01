8d0b338e98e6c656d2f32a73b91b202564683602
stella <khk9562@naver.com>
2024-05-01 18:41:23 +0900

    styled-components 글로벌 스타일 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+
+import { ThemeProvider } from "styled-components";
+import theme from "./shared/\btheme";
+import { GlobalStyle } from "./shared/global";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
