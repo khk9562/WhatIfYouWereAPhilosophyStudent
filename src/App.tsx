f368450f80b4fed94a0aea66837edcef5336475e
stella <khk9562@naver.com>
2024-05-11 15:29:03 +0900

    react-ga4 설치 후 GA 측정 ID로 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Select from "./pages/Select";
 import Result from "./pages/Result";
+import ReactGA from "react-ga4";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(`${process.env.REACT_PUBLIC_GA_ID}`);
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Main />} />
