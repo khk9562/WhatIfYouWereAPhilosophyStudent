30c527b39b987f81219f87961d4810b19ef134ea
stella <khk9562@naver.com>
2024-05-18 20:54:18 +0900

    질문 부분 디자인 수정

--- src/components/Question/style.tsx
+++ src/components/Question/style.tsx
@@ -2,12 +2,15 @@ import styled, { css } from "styled-components";
 
 const StyledQuestion = styled.h1`
   font-weight: 500;
-  padding: 0.75rem 2rem;
+  width: 100%;
+  min-height: 3.75rem;
+  padding: 1.25rem 2rem;
   background-color: var(--point);
   color: rgb(249 250 251);
-  line-hight: 1.25rem;
-  font-size: 0.875rem;
+  line-hight: 3.75rem;
+  font-size: 0.95rem;
   border-radius: 10px;
+  margin-bottom: 12px;
 `;
 
 export default StyledQuestion;
